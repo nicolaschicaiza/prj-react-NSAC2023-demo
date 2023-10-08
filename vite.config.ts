505313646191db8c3ba0ad3b73df0546cd792502
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-10-08 18:00:41 -0500

    :sparkles: (NICO) Preparado para desplegar

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 4000, // you can replace this port with any port
-  }
+  base: "prj-react-NSAC2023-demo",
+  plugins: [react()]
 })
